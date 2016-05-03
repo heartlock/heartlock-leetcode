28a37eb2047a43f1326cc8f8cddc63d9ac7e9f33
heartlock <21521209@zju.edu.cn>
2016-05-03 16:47:55 +0800

    2016050316

---
+++ 69.cpp
@@ -0,0 +1,43 @@
+class Solution {
+
+public:
+
+    int mySqrt(int x) {
+
+      unsigned long long begin = 0;
+
+        unsigned long long end = (x+1)/2;
+
+        unsigned long long mid;
+
+        unsigned long long tmp;
+
+        while(begin < end)
+
+        {
+
+            mid = begin + (end-begin)/2;
+
+            tmp = mid*mid;
+
+            if(tmp==x)return mid;
+
+            else if(tmp<x) begin = mid+1;
+
+            else end = mid-1;
+
+        }
+
+        tmp = end*end;
+
+        if(tmp > x)
+
+            return end-1;
+
+        else
+
+            return end;
+
+    }
+
+};
